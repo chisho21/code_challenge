855cfc1fd478943ea1a110b0d09937bc8a5ae526
Evan Chisholm <chisho21@gmail.com>
2021-10-26 10:00:50 -0400

    Speed optimized to pull ALL permeatations.

--- chessboard.ps1
+++ chessboard.ps1
@@ -1,145 +1,166 @@
-## Make chess board array blanks will be 0's
-$table =  @(
-    ('a','b','c','0','e'),
-    ('0','g','h','i','j'),
-    ('k','l','m','n','o'), 
-    ('p','q','r','s','t'),
-    ('u','v','0','0','y')
-)
+#!/bin/pwsh
 
-# Function to resolve a letter to coordinates
-function getCoordinate {
-    param (
-        [ValidatePattern("^[a-zA-Z]$")][string]
-        $Letter,
-        [array]$Table = $Table
+$scriptblock = {
+    ## Make chess board array blanks will be 0's
+    $table =  @(
+        ('a','b','c','0','e'),
+        ('0','g','h','i','j'),
+        ('k','l','m','n','o'), 
+        ('p','q','r','s','t'),
+        ('u','v','0','0','y')
     )
-    # Get Coordinate of start
-    ### RFI: Where-Object is inefficient (even by powershell standards)
-    $row = ($table | Where-Object {$_ -like "*$Letter*"})
-    if (!$row){
-        Write-Error "$letter not found in table"
-        return
-    }
-    $rownum = $table.IndexOf($row)
-    $colnum = $row.IndexOf($LETTER)
 
-    #eturn "$rownum,$colnum"
-    @{y = $rownum; x = $colnum}
-   
-}
-# Function to resolve coordinates to a letter
-function getLetter {
-    param (
-        [int]$Y,
-        [int]$X,
-        [array]$Table = $Table
-    )
-    # uncomment for tshooting
-    #write-host "Trying X: $X and Y: $Y"
-    $table[($Y)][($X)]
+    # Function to resolve a letter to coordinates
+    function getCoordinate {
+        param (
+            [ValidatePattern("^[a-zA-Z]$")][string]
+            $Letter,
+            [array]$Table = $Table
+        )
+        # Get Coordinate of start
+        ### RFI: Where-Object is inefficient (even by powershell standards)
+        $row = ($table | Where-Object {$_ -like "*$Letter*"})
+        if (!$row){
+            Write-Error "$letter not found in table"
+            return
+        }
+        $rownum = $table.IndexOf($row)
+        $colnum = $row.IndexOf($LETTER)
+
+        #eturn "$rownum,$colnum"
+        @{y = $rownum; x = $colnum}
     
-}
-
-## function to determine valid knight moves on the board
-function validMoveEnumerator {
-    param (
-        [ValidatePattern("^[a-zA-Z]$")][string]
-        $Letter,
-        [array]$Table = $Table
-    )
-    $maxy = $table.count -1
-    $maxx = $table[0].count -1
-
-    $coordinate = getCoordinate $Letter
-    if (!$coordinate){
-        Write-Error "$letter not found with getCoordinate"
-        return
+    }
+    # Function to resolve coordinates to a letter
+    function getLetter {
+        param (
+            [int]$Y,
+            [int]$X,
+            [array]$Table = $Table
+        )
+        # uncomment for tshooting
+        #write-host "Trying X: $X and Y: $Y"
+        $table[($Y)][($X)]
+        
     }
 
-    # Check all possibilities
-    $validmoves = @()
-    $knightmoves = @( '1,2','2,1','-1,2','-2,1','1,-2','2,-1','-1,-2','-2,-1' )
-    foreach ($move in $knightmoves){
-        $movey = $move.split(',')[0]
-        $movex = $move.split(',')[1]
-        ### RFI: could make a coordinate class and do it "right"
-        $newy = $coordinate.y + $movey
-        $newx = $coordinate.x + $movex
+    ## function to determine valid knight moves on the board
+    function validMoveEnumerator {
+        param (
+            [ValidatePattern("^[a-zA-Z]$")][string]
+            $Letter,
+            [array]$Table = $Table
+        )
+        $maxy = $table.count -1
+        $maxx = $table[0].count -1
 
-        if ($newx -ge 0 -and $newx -le $maxx -and $newy -ge 0 -and $newy -le $maxy ){
-            $let = getLetter -y $newy -x $newx
-            if ($let -notlike "0"){
-                $validmoves += $let
-            }
-        }
-        else {
-            Write-Verbose "Move [] $move ] is off the board"
+        $coordinate = getCoordinate $Letter
+        if (!$coordinate){
+            Write-Error "$letter not found with getCoordinate"
+            return
         }
 
-    }
-    $validmoves
-}
+        # Check all possibilities
+        $validmoves = @()
+        $knightmoves = @( '1,2','2,1','-1,2','-2,1','1,-2','2,-1','-1,-2','-2,-1' )
+        foreach ($move in $knightmoves){
+            $movey = $move.split(',')[0]
+            $movex = $move.split(',')[1]
+            ### RFI: could make a coordinate class and do it "right"
+            $newy = $coordinate.y + $movey
+            $newx = $coordinate.x + $movex
 
-function SovleMatrix {
-    param (
-        [ValidatePattern("^[a-zA-Z]$")][string]
-        $Letter,
-        [int]$MaxMoves = 8,
-        [int]$MaxVowels = 2,
-        $NoTriples = @('a','e','i','o','u','y'),
-        [array]$Table = $Table,
-        [switch]$ShowValues
-    )
-    $levels = @()
-    $move = 1
-    Do {
-
-        #first loop
-        ### RFI: I'm sure there's logic to not have separate loops but quick and dirty will have to do for now.
-        if ($move -eq 1){
-            $valids = validMoveEnumerator -Letter $Letter -Table $Table
-            foreach ($valid in $valids){
-                $levels += "$Letter$valid"
-            }
-        }
-        else {
-            foreach ($level in $levels){
-                $valids = validMoveEnumerator -Letter $level[-1] -Table $Table
-                foreach ($valid in $valids){
-                    $levels += "$Level$valid"
+            if ($newx -ge 0 -and $newx -le $maxx -and $newy -ge 0 -and $newy -le $maxy ){
+                $let = getLetter -y $newy -x $newx
+                if ($let -notlike "0"){
+                    $validmoves += $let
                 }
             }
+            else {
+                Write-Verbose "Move [] $move ] is off the board"
+            }
+
         }
-        
-    $move ++
-    }until ($move -gt $MaxMoves)
-    $levels = $levels | Where-Object {$_.length -eq $maxmoves + 1} | Select-Object -Unique
-    ## Do the Vowel check
-    $finallevels = @()
-    foreach ($level in $levels){
-        $vcount = 0
-        foreach ($l in $level.ToCharArray()){
-            ### RFI: Regex match would be faster, but need to learn black magic first.
-            if ($notriples -contains $l){
-                $vcount ++
+        $validmoves
+    }
+
+
+    function SovleMatrix {
+        param (
+            [ValidatePattern("^[a-zA-Z]$")][string]
+            $Letter,
+            [int]$MaxMoves = 8,
+            [int]$MaxVowels = 2,
+            $NoTriples = @('a','e','i','o','u','y'),
+            [array]$Table = $Table,
+            [switch]$ShowValues
+        )
+        $levels = @()
+        $move = 1
+        Do {
+
+            #first loop
+            ### RFI: I'm sure there's logic to not have separate loops but quick and dirty will have to do for now.
+            if ($move -eq 1){
+                $valids = validMoveEnumerator -Letter $Letter -Table $Table
+                foreach ($valid in $valids){
+                    $levels += "$Letter$valid"
+                }
+            }
+            else {
+                $templevel = @()
+                foreach ($level in $levels){
+                    $valids = validMoveEnumerator -Letter $level[-1] -Table $Table
+                    foreach ($valid in $valids){
+                        $templevel  += "$Level$valid"
+                    }
+                }
+                $levels = $templevel
+            }
+            
+        $move ++
+        }until ($move -gt $MaxMoves)
+        ## remove dupes
+        $levels = $levels | Select-Object -Unique
+        ## Do the Vowel check
+        $finallevels = @()
+        foreach ($level in $levels){
+            $vcount = 0
+            foreach ($l in $level.ToCharArray()){
+                ### RFI: Regex match would be faster, but need to learn black magic first.
+                if ($notriples -contains $l){
+                    $vcount ++
+                }
+            }
+            if ($vcount -le $MaxVowels){
+                $finallevels += $level
             }
         }
-        if ($vcount -le $MaxVowels){
-            $finallevels += $level
+        if ($ShowValues){
+            $finallevels
         }
+        else {
+            $finallevels.count
+        }
+        
     }
-    if ($ShowValues){
-        $finallevels
-    }
-    else {
-        $finallevels.count
-    }
-    
-}
+    SovleMatrix -Letter $_ -MaxMoves 7
+}# end scriptblock
 
 
 # Performance Test
 # Measure-Command -Expression { SovleMatrix -Letter a -MaxMoves 8 | Out-Default}
+$list = @()
+foreach ($rows in $table){
+    foreach ($r in $rows){
+        if ($r -ne "0"){
+            $list += $r
+        }
+    }
+}
 
-SovleMatrix -Letter a -MaxMoves 8
+$total = $list | ForEach-Object -Parallel $scriptblock
+
+$final = 0 
+$total | ForEach-Object { $final += $_}
+$final
